d70e650794714debbb165b15e3970165300cc898
Milos Subotic <milos.subotic.sm@gmail.com>
2020-03-14 21:15:39 +0100

    Corrections.

--- FW/app_03_stopwatch/app_03_stopwatch.c
+++ FW/app_03_stopwatch/app_03_stopwatch.c
@@ -3,30 +3,10 @@
 #include <stdint.h>
 #include "system.h"
 
-void busy_wait(int ms){
-	//TODO
-}
 
 int main() {
-	volatile uint32_t* pio = (volatile uint32_t*)SW_AND_LED_PIO_BASE;
-
-	uint8_t reset; // SW[0]
-	uint8_t pause; // SW[1]
-	uint8_t moduo; // SW[7:2]
-
-	uint8_t cnt;
-
-
-	while(1) { // Infinite loop.
-		// Read inputs.
-		//TODO Прочитати SW у reset, pause, moduo.
-
-		// Calculate state.
-		//TODO Срачунати стање cnt.
-
-		// Write outputs.
-		//TODO Уписати резултат у LED.
-	}
+	//TODO Направити бројач, користећи функционалности периферија одговарајућег термина,
+	// како је већ дефинисано у задатку у PDF-у вежбе.
 
 	return 0;
 }
