d5666acebc253d3d04c7f2b19ed10f8b37148b43
Milos Subotic <milos.subotic.sm@gmail.com>
2020-03-14 21:53:14 +0100

    Done apps.

--- FW/app_02_interrupt/app_02_interrupt.c
+++ FW/app_02_interrupt/app_02_interrupt.c
@@ -5,24 +5,58 @@
 #include "sys/alt_irq.h"
 #include <stdio.h>
 
-#define WAIT_UNITL_0(x) while(x != 0){}
-#define WAIT_UNITL_1(x) while(x != 1){}
+#define WAIT_UNITL_FALSE(x) while((x)){}
+#define WAIT_UNITL_TRUE(x) while(!(x)){}
 
-static volatile uint32_t* pio = (volatile uint32_t*)SW_AND_LED_PIO_BASE;
+typedef struct {
+	// reg 0
+	unsigned sw0_led0 : 1;
+	unsigned _res0 : 31;
+	unsigned sw1_led1 : 1;
+	unsigned _res1 : 31;
+	unsigned sw2_led2 : 1;
+	unsigned _res2 : 31;
+	unsigned sw3_led3 : 1;
+	unsigned _res3 : 31;
+	unsigned sw4_led4 : 1;
+	unsigned _res4 : 31;
+	unsigned sw5_led5 : 1;
+	unsigned _res5 : 31;
+	unsigned sw6_led6 : 1;
+	unsigned _res6 : 31;
+	unsigned sw7_led7 : 1;
+	unsigned _res7 : 31;
+	// reg 8
+	unsigned sw_led : 8;
+	unsigned _res8 : 24;
+	// reg 9
+	unsigned sw_changed : 8;
+	unsigned _res9 : 24;
+	// reg 10
+	unsigned sw_set_leds : 1;
+	unsigned _res10 : 31;
+	// reg 11
+	uint32_t invert_leds;
+	uint32_t babadeda0;
+	uint32_t babadeda1;
+	uint32_t babadeda2;
+	uint32_t babadeda3;
+
+} bf_pio;
+#define pio (*((volatile bf_pio*)SW_AND_LED_PIO_BASE))
+
 static volatile uint32_t* digits = (volatile uint32_t*)TIME_MUXED_7SEGM_BASE;
 static volatile uint32_t* timer = (volatile uint32_t*)TIMER_BASE;
 
 static void timer_isr(void * context, alt_u32 id) {
-	//timer[0] = 0;
 	static uint8_t x = 0;
 	x++;
-	if(x == 16){
-		x = 0;
-	}
-	pio[8] = x;
+	pio.sw_led = x;
 }
 
 int main() {
+	pio.sw_led = 0x81; // For debugging purposes.
+
 	// Init IRQ.
 	alt_ic_isr_register(
 		TIMER_IRQ_INTERRUPT_CONTROLLER_ID, //alt_u32 ic_id
@@ -32,7 +66,6 @@ int main() {
 		NULL //void *flags
 	);
 
-	pio[8] = 0x81; // For debugging.
 
 	timer[1] = 12000000; // modulo.
 	timer[2] = 0; // Start it.
